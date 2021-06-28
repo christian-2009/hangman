80fed65e96e7bc67dc7a49985c61bc54e496d6cb
christian-2009 <85346878+christian-2009@users.noreply.github.com>
2021-06-28 22:15:57 +0100

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -42,7 +42,7 @@ def hangman(word):
         #need to check whether any letters corresponded to a letter in word
         if a == 0:
             lives +=1
-            print("You guessed incorrectly. One life gone.")
+            print("You guessed incorrectly " + str(10 - lives) + " lives remaining.")
         elif a > 0:
             print("Well done you guessed a correct letter. Now guess from " + lng + ".")
         else:
@@ -52,7 +52,7 @@ def hangman(word):
         print("Game over. You lose!")
     return word
 
-hangman("happy")
+hangman("daniel")
 
 
 
